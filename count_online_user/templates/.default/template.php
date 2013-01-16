c1b66d418fe295bc81e9d90ccd740f8a974c5bd5
Sergey <sergey.greznov@gmial.com>
2013-01-16 16:44:44 +0400

    .

--- count_online_user/templates/.default/template.php
+++ count_online_user/templates/.default/template.php
@@ -1,5 +1,12 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<span class="site-selector-menu">
-	<span class="site-selector-menu-text">Пользователей онлайн: <?=count($arResult["USERS_AUTH"]);?></span>
-</span>
-<span class="site-selector-separator"></span>
+<div class="online_user_wrapper">
+	<b class="r2"></b><b class="r1"></b><b class="r0"></b>
+	<div class="online_user_inner">
+		<div class="online_user_content">
+			<span class="online_user_content_bolder">Сегодня:</span> <?=date('d.m.Y')?>
+			<br />
+			<span class="online_user_content_bolder">Пользователей онлайн:</span> <?=count($arResult["ONLINE_USERS"]);?>
+		</div>
+	</div>
+	<i class="r0"></i><i class="r1"></i><i class="r2"></i>
+</div>
