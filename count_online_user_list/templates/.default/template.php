a7eb9a4535257e468e3392b4f1d8a6c1d61f282c
Sergey <sergey.greznov@gmial.com>
2013-01-24 14:01:17 +0400

    o

--- count_online_user_list/templates/.default/template.php
+++ count_online_user_list/templates/.default/template.php
@@ -1,12 +1,41 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<div class="online_user_wrapper">
-	<b class="r2"></b><b class="r1"></b><b class="r0"></b>
-	<div class="online_user_inner">
-		<div class="online_user_content">
-			<?foreach ($arResult as $key => $arUser) {?>
-				<span class="online_user_content_bolder">Пользователей онлайн: (<?=$key?>)</span> <?=print_r($arUser,1);?><?='<br>'?>
-			<?}?>
-		</div>
-	</div>
-	<i class="r0"></i><i class="r1"></i><i class="r2"></i>
-</div>
+
+<div><?=$arResult['NAV']?></div>
+
+<?
+
+$arParams = $arResult['PARAMS'];
+
+foreach ($arResult['USERS'] as $key => $arUser) {
+
+	$APPLICATION->IncludeComponent(
+		'bitrix:intranet.system.person',
+		'.default',
+		array(
+			'USER' => $arUser,
+			'USER_PROPERTY' => $arParams['USER_PROPERTY'],
+			'PM_URL' => $arParams['PM_URL'],
+			'STRUCTURE_PAGE' => $arParams['STRUCTURE_PAGE'],
+			'STRUCTURE_FILTER' => $arParams['STRUCTURE_FILTER'],
+			'USER_PROP' => $arResult['USER_PROP'],
+			'NAME_TEMPLATE' => $arParams['NAME_TEMPLATE'],
+			'SHOW_LOGIN' => $arParams['SHOW_LOGIN'],
+			"DATE_FORMAT" => $arParams["DATE_FORMAT"],
+			"DATE_FORMAT_NO_YEAR" => $arParams["DATE_FORMAT_NO_YEAR"],
+			"DATE_TIME_FORMAT" => $arParams["DATE_TIME_FORMAT"],
+			"SHOW_YEAR" => $arParams["SHOW_YEAR"],
+			"CACHE_TYPE" => $arParams["CACHE_TYPE"],
+			"CACHE_TIME" => $arParams["CACHE_TIME"],
+			"PATH_TO_CONPANY_DEPARTMENT" => $arParams["~PATH_TO_CONPANY_DEPARTMENT"],
+			"PATH_TO_VIDEO_CALL" => $arParams["~PATH_TO_VIDEO_CALL"],
+		),
+		null,
+		array('HIDE_ICONS' => 'Y')
+	);
+
+}
+
+?>
+
+
+<div><?=$arResult['NAV']?></div>
