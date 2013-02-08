62dd4cb7cda2a5d04af831623c951c834451f535
SerjRamone <sergey.greznov@gmial.com>
2013-02-08 13:19:49 +0400

    update

---
+++ gadget/usersonline/lang/ru/.parameters.php
@@ -0,0 +1,5 @@
+<?
+$MESS ['GD_BIRTHDAY_P_URL'] = "URL страницы со списком юбилеев";
+$MESS ['GD_BIRTHDAY_P_ALL'] = "(все подразделения)";
+$MESS ['GD_BIRTHDAY_P_DEP'] = "Показывать для отдела/офиса";
+?>
